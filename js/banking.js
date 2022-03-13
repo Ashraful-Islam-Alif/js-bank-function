feef2b3013779f6cb0b4c47c024767d7669ecfee
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-03-13 20:01:56 +0600

    error handling

--- js/banking.js
+++ js/banking.js
@@ -3,6 +3,7 @@ let depositeTotal = document.getElementById('deposit-total')
 let withdrawInput = document.getElementById('withdraw-input');
 let withdrawTotal = document.getElementById('withdraw-total')
 let balanceTotal = document.getElementById('balance-total');
+
 // deposite button with onclick
 function depositeButton() {
     //let depositButton = document.getElementById('deposit-button');
@@ -10,23 +11,27 @@ function depositeButton() {
     /* depositeTotal.innerText = parseFloat(depositeTotal.innerText) + parseFloat(input.value) */
 
     const totalBalance = addMoney(depositeTotal.innerText, input.value)
-    depositeTotal.innerText = totalBalance;
-    /* 
+    if (totalBalance > 0) {
+        depositeTotal.innerText = totalBalance;
+        /* 
     balanceTotal.innerText = parseFloat(balanceTotal.innerText) + parseFloat(input.value) */
-    const displayBalanceTotal = addMoney(balanceTotal.innerText, input.value)
-    balanceTotal.innerText = displayBalanceTotal
+        const displayBalanceTotal = addMoney(balanceTotal.innerText, input.value)
+        balanceTotal.innerText = displayBalanceTotal
+
+    }
     input.value = ''
 }
 
 
 // withdraw button with addEventListener
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    //let depositButton = document.getElementById('withdraw-button');
     const totalWithdraw = addMoney(withdrawTotal.innerText, withdrawInput.value)
-    withdrawTotal.innerText = totalWithdraw;
-    // withdrawTotal.innerText = parseFloat(withdrawTotal.innerText) + parseFloat(withdrawInput.value)
-    const balanceWithdraw = subMoney(balanceTotal.innerText, withdrawInput.value)
-    balanceTotal.innerText = balanceWithdraw;
+    if (totalWithdraw > 0 && balanceTotal.innerText > withdrawInput.value) {
+        withdrawTotal.innerText = totalWithdraw;
+        // withdrawTotal.innerText = parseFloat(withdrawTotal.innerText) + parseFloat(withdrawInput.value)
+        const balanceWithdraw = subMoney(balanceTotal.innerText, withdrawInput.value)
+        balanceTotal.innerText = balanceWithdraw;
+    }
     // balanceTotal.innerText = parseFloat(balanceTotal.innerText) - parseFloat(withdrawInput.value)
 
     withdrawInput.value = ''
